2c475e5773589775de1b60ccf08ccfeccfa11fc1
HireSmartTT <extrahandstt@gmail.com>
2026-06-13 00:03:16 -0400

    Fix React root rendering and Helmet setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 import "./styles/global.css";
 import { HelmetProvider } from "react-helmet-async";
 
-root.render(
-  <HelmetProvider>
-    <App />
-  </HelmetProvider>
-);
-
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
+  </StrictMode>
+);
